53455294a99ebe041619f8d14470731395f4b4de
Oleksandr Ronskyi <oleksandr.ronskyi@gmail.com>
2022-10-22 14:11:28 -0400

    fixed problem with devserver

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,10 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-    }
+    },
+     devServer: {
+            static: {
+                directory: path.join(__dirname, '/')
+            }
+        },
 }
